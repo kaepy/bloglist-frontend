711a9ea5ebb077196c89e039bbbb119a37b2313d
kaepy <henna.tuunainen@gmail.com>
2023-06-11 22:35:55 +0300

    Add 5.10 blogilistan frontend, step 10

--- src/components/Bloglist.js
+++ src/components/Bloglist.js
@@ -6,12 +6,12 @@ const Bloglist = ({ blogFormRef, user, blogs, logout, createBlog, updateBlog }) 
 
   return (
     <div>
-    <p>{user.name} logged in <button onClick={logout}>logout</button></p>
-    { blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      )
-    }
-    <Togglable buttonLabel='new blog' ref={blogFormRef}><BlogForm createBlog={createBlog} /></Togglable>
+      <p>{user.name} logged in <button onClick={logout}>logout</button></p>
+      {blogs
+        .sort((a, b) => a.likes - b.likes)
+        .map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />)
+      }
+      <Togglable buttonLabel='new blog' ref={blogFormRef}><BlogForm createBlog={createBlog} /></Togglable>
     </div>
   )
 }
