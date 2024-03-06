3329389dd3421c2e88fb8892111245e394ff5cbf
kaepy <henna.tuunainen@gmail.com>
2024-03-06 17:53:34 +0200

    5.18 blogilistan E2E -testit, step 2 Cypress: onnistuneen ja epäonnistuneen kirjautumisen testaus

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,10 +1,14 @@
-// 5.17: blogilistan end to end ‑testit, step1
-// Konfiguroi Cypress projektiisi. Tee testi, joka varmistaa, että sovellus näyttää oletusarvoisesti kirjautumislomakkeen.
-// Testin beforeEach-alustuslohkon tulee nollata tietokannan tilanne.
+// 5.18: blogilistan end to end ‑testit, step2
+// Tee testit kirjautumiselle, testaa sekä onnistunut että epäonnistunut kirjautuminen. Luo testejä varten käyttäjä beforeEach-lohkossa.
 
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      username: 'himmeli',
+      password: 'hommeli'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -12,6 +16,24 @@ describe('Blog app', function () {
     cy.contains('username').get('#username')
     cy.contains('password').get('#password')
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('himmeli')
+      cy.get('#password').type('hommeli')
+      cy.get('#login-button').click()
+
+      cy.contains('Welcome himmeli!')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('himmeli')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.contains('Ups! Wrong credentials. Try again :)')
+    })
   })
 
 })
